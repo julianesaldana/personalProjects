107db476f7f2e2c3e7c43dbdce46a92591e5be8d
Julian Saldana <julianesaldana@yahoo.com>
2021-09-16 10:26:00 -0700

    small progress

--- SpreadsheetEditor/main.py
+++ SpreadsheetEditor/main.py
@@ -1,5 +1,13 @@
-def main():
-    print("Hello")
+from openpyxl import load_workbook
 
+def main():
+    # loading excel file
+    workbook = load_workbook(filename="sample.xlsx")
+
+    #opening workbook
+    sheet = workbook.active
+
+    # modifying the desired cell
+    sheet["A1"] = "Full Name"
 
 main()
