043dd170a442d7ea9d82d4b76942aaa3c99c629d
Julian Saldana <juliansaldana@Julians-MBP.lan>
2021-10-21 15:04:36 -0700

    directory and organizational changes

--- java stuff/test/src/Tester.java
+++
@@ -1,9 +0,0 @@
-public class Tester {
-    public static void main(String[]arg) {
-        Student student1 = new Student("firstName", 3.9);
-        student1.display();
-
-        Student student2 = new Student("secondName", 3.75);
-        student2.display();
-    }
-}
